d1827f6ada8ccc9df5d01942311e971d24ca2eeb
TecAbel <abelardo96.work@gmail.com>
2019-09-24 17:30:21 -0500

    footer en repetitivo

--- index.php
+++ index.php
@@ -33,48 +33,7 @@
             </div>
         </form>
     </div>
-    <footer class="page-footer font-small bg-dark pt-4">
-        <div class="container-fluid text-center text-md-left">
-            <div class="row">
-                <div class="col-md-6 mt-md-0 mt-3">
-                    <h5 class="text-uppercase">Contacto</h5>
-                    <p>Eje central N. XXXX, Niños Héroes, Benito juárez, CDMX <i class="fas fa-map-marker-alt"></i></p>
-                </div>
-                <hr class="clearfix w-100 d-md-none pb-3">
-                <div class="col-md-3 mb-md-0 mb-3">
-                    <h5 class="text-uppercase">Correos</h5>
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#">ingjagm@gmail.com</a>
-                        </li>
-                        <li>
-                            <a href="#">ingjagm@hotmail.com</a>
-                        </li>
-                        <li>
-                            <a href="#">ingjagm@yahoo.com.mx</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-3 mb-md-0 mb-3">
-                    <h5 class="text-uppercase">Teléfono</h5>
-                    <ul class="list-unstyled">
-                        <li>
-                            <p>55-55-55-55</p>
-                        </li>
-                        <li>
-                            <p>55-55-55-55</p>
-                        </li>
-                        <li>
-                            <p>55-55-55-55</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright text-center py-3">© 2019 Copyright:
-            <a href="#"> Abelardo Aqui Arroyo</a>
-        </div>
-    </footer>
+    <?php echo getFooter() ?>
     <script src="recursosJS/animated-input.js"></script>
 </body>
 </html>
