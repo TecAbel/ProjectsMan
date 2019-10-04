e0f7d62e040072bf1d0faeae12c0bd0553777da1
TecAbel <abelardo96.work@gmail.com>
2019-10-04 13:31:40 -0500

    insert proyecto junto con avance

--- PHP/registroProyecto.php
+++ PHP/registroProyecto.php
@@ -4,11 +4,16 @@
         $msg = "hubo un error, intente de nuevo";
     }
     else{
+        $fecha = date("Y-m-d");
         $nombreProyecto = $_POST['txtNombreProyecto'];
         $sql = "INSERT INTO proyectos(nombre_proyecto) VALUES ('$nombreProyecto');";
+        $sql2 = "INSERT INTO avances(num_proyecto,num_usuario,fecha,nombre_detalle)
+        VALUES ((SELECT * FROM ultimoproyecto),'17', '$fecha', 'Se creó proyecto');";
 
         if(mysqli_query($conn, $sql)){
-            $msg = true;
+            if(mysqli_query($conn, $sql2)){
+                $msg = true;
+            }
         }
         else{
             $msg = mysqli_error($conn);
