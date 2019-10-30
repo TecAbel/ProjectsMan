52922692225e65ce2b78e163f185e175e1398240
TecAbel <abelardo96.work@gmail.com>
2019-10-30 12:13:35 -0600

    error de carga de ultimoproyecto

--- PHP/registroProyecto.php
+++ PHP/registroProyecto.php
@@ -12,7 +12,7 @@
         /*$sql2 = "INSERT INTO avances(num_proyecto,num_usuario,fecha,nombre_detalle)
         VALUES ((SELECT * FROM ultimoproyecto),(SELECT num_usuario FROM usuarios WHERE correo = '$correo'), '$fecha', 'Se creó proyecto');";*/
         $sql2 = "INSERT INTO avances(num_proyecto,num_usuario,fecha,nombre_detalle, detalle_avance)
-        VALUES ((SELECT * FROM ultimoproyecto),(SELECT num_usuario FROM usuarios WHERE correo = '$correo'), '$fecha', 'Inicio de proyecto', 'Se creó proyecto')";
+        VALUES ((SELECT * FROM ultimoProyecto),(SELECT num_usuario FROM usuarios WHERE correo = '$correo'), '$fecha', 'Inicio de proyecto', 'Se creó proyecto')";
 
         if(mysqli_query($conn, $sql)){
             if(mysqli_query($conn, $sql2)){
